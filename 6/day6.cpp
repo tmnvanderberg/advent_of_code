f681ddba3d5858fbe3b47a9188ed1d48050ad320
Timon van der Berg <timon.vanderberg@streamunlimited.com>
2024-12-06 20:11:53 +0100

    day 6: part 2

--- 6/day6.cpp
+++ 6/day6.cpp
@@ -1,277 +1,193 @@
 #include <iostream>
 #include <vector>
-#include <array>
 #include <algorithm>
-#include <optional>
+#include <map>
 #include <fstream>
-#include <numeric>
-
-constexpr char _clear_tile_ {'.'};
-
-constexpr char _guard_up_ {'^'};
-constexpr char _guard_left_ {'<'};
-constexpr char _guard_right_ {'>'};
-constexpr char _guard_down_ {'v'};
-
-constexpr std::array<char,4> _guard_symbols_ = {_guard_up_, _guard_left_, _guard_right_, _guard_down_};
 
 constexpr char _obstruction_ {'#'};
 constexpr char _visited_tile_ {'X'};
-
-constexpr char _my_obstruction_{'#'};
-
-enum class orientation {
-	up,
-	right,
-	down,
-	left 
-};
-
-struct starting_pos_and_orientation {
-	int row;
-	int col;
-	orientation ori;
-};
-
-void print_map(const std::vector<std::string>& map){
-	std::cout << "-----map------" << '\n';
-	for (const auto& row: map){
-		std::cout << row << std::endl;
-	}
-}
+constexpr char _clear_tile_ {'.'};
 
 enum class result {
-	incremented,
+	moved,
 	ended,
 	loop
 };
 
-bool have_i_looped(const int row, const int col, const orientation ori, const starting_pos_and_orientation &spao) {
-	const bool ret = (spao.row == row && spao.col == col && spao.ori == ori);
-	if (ret) {
-		std::cout << "you have looped!" << std::endl;
-	}
-	return ret;
-}
+enum class direction {
+	up,
+	down,
+	left,
+	right
+};
 
-result increment_guard_position(std::vector<std::string> &map, const starting_pos_and_orientation &spao, bool* starting) {
-	std::cout << ((*starting) ? "starting" : "not starting") << std::endl;
-	const int row_size = static_cast<int>(map.size());
-	const int col_size = static_cast<int>(map[0].size());
-	for (int row_idx = 0; row_idx != row_size; ++row_idx) {
-		for (int col_idx = 0; col_idx != col_size; ++col_idx) {
-			const auto symbol = map[row_idx][col_idx];
-			const auto guard_symbol_it = std::find(_guard_symbols_.cbegin(), _guard_symbols_.cend(), symbol);
-			if (guard_symbol_it != _guard_symbols_.cend()) {
-				const char guard_symb = *guard_symbol_it;
-				std:: cout << "guard_symb: " << guard_symb << std::endl;
-				switch(guard_symb) {
-					case _guard_up_:
-						{
-							std::cout << "guard up at (" << row_idx << ", " << col_idx << ")" << std::endl;
-							const int new_row_idx = static_cast<int>(row_idx) - 1;
-							if (new_row_idx >= 0 && new_row_idx < row_size) { 
-								const char next = map[new_row_idx][col_idx];
-								std::cout << "next " << next << " at (" << new_row_idx << "," << col_idx << std::endl;
+const std::map<const char, const direction> guard_char_directions = {
+	{'^', direction::up},
+	{'v', direction::down},
+	{'<', direction::left},
+	{'>', direction::right},
+};
 
-								// loop can mean: we turned last time and are now at starting pos and orientation
-								// OR: we are moving into starting pos now
-								// but it should not be our starting pos.
-								if(
-										!*starting 
-										&& (have_i_looped(row_idx, col_idx, orientation::up, spao) 
-										|| have_i_looped(new_row_idx, col_idx, orientation::up, spao)
-										)
-										) 
-								{
-									return result::loop;
-								}
+struct place {
+	int row;
+	int col;
+	direction dir;
+};
 
-								if (next != _obstruction_){
-										// move
-										std::cout << "=> clear tile" << std::endl;
-										map[new_row_idx][col_idx] = _guard_up_;
-										map[row_idx][col_idx] = _visited_tile_;
-								}
-								if (next == _obstruction_) {
-									// turn right, don't move
-									std::cout << "obstruction" << std::endl;		
-									map[row_idx][col_idx] = _guard_right_;
-								}
-								return result::incremented;
-							}
-							return result::ended;
-						}
-					case _guard_left_:
-						{
-							std::cout << "guard left at (" << row_idx << ", " << col_idx << ")" << std::endl;
-							const int new_col_idx = static_cast<int>(col_idx) - 1;
-							if (new_col_idx >= 0 && new_col_idx < col_size) { 
-								const char next = map[row_idx][new_col_idx];
-								std::cout << "next " << next << " at (" << row_idx << "," << new_col_idx << std::endl;
+class guard {
+	public:
 
-								if(
-										!*starting 
-										&& (have_i_looped(row_idx, col_idx, orientation::left, spao) 
-										|| have_i_looped(row_idx, new_col_idx, orientation::left, spao))
-										) 
-								{
-									return result::loop;
-								}
+		guard(
+				std::vector<std::string> map) 
+			: 
+					_history()
+				,	_map(map)
+		{
+				_rsize = map.size();
+				_csize = map[0].size();
+				find_start();
+		};
 
-								if (next != _obstruction_){
-										std::cout << "=> clear tile" << std::endl;
-										map[row_idx][new_col_idx] = _guard_left_;
-										map[row_idx][col_idx] = _visited_tile_;
-								}
-								if (next == _obstruction_) {
-									// turn right, don't move
-									std::cout << "obstruction" << std::endl;		
-									map[row_idx][col_idx] = _guard_up_;
-								}
-								return result::incremented;
-							}
-							return result::ended;
-						}
-					case _guard_right_:
-						{
-							std::cout << "guard right at (" << row_idx << ", " << col_idx << ")" << std::endl;
-							const int new_col_idx = static_cast<int>(col_idx) + 1;
-							if (new_col_idx >= 0 && new_col_idx < col_size) { 
-								const char next = map[row_idx][new_col_idx];
-								std::cout << "next " << next << " at (" << row_idx << "," << new_col_idx << std::endl;
-
-								if(
-										!*starting &&
-										(have_i_looped(row_idx, col_idx, orientation::right, spao) 
-										|| have_i_looped(row_idx, new_col_idx, orientation::right, spao))
-										) 
-								{
-									return result::loop;
-								}
-
-								if (next != _obstruction_){
-										std::cout << "=> clear tile" << std::endl;
-										map[row_idx][new_col_idx] = _guard_right_;
-										map[row_idx][col_idx] = _visited_tile_;
-								}
-								if (next == _obstruction_) {
-									// turn right, don't move
-									std::cout << "obstruction" << std::endl;		
-									map[row_idx][col_idx] = _guard_down_;
-								}
-								return result::incremented;
-							}
-							return result::ended;
-						}
-					case _guard_down_:
-						{
-							std::cout << "guard down at (" << row_idx << ", " << col_idx << ")" << std::endl;
-							const int new_row_idx = static_cast<int>(row_idx) + 1;
-							if (new_row_idx >= 0 && new_row_idx < row_size) { 
-								const char next = map[new_row_idx][col_idx];
-								std::cout << "next " << next << " at (" << new_row_idx << "," << col_idx << std::endl;
-
-								if(
-										!*starting &&
-										(have_i_looped(row_idx, col_idx, orientation::down, spao) 
-										|| have_i_looped(new_row_idx, col_idx, orientation::down, spao))
-										) 
-								{
-									return result::loop;
-								}
-
-								if (next != _obstruction_){
-										std::cout << "=> clear tile" << std::endl;
-										map[new_row_idx][col_idx] = _guard_down_;
-										map[row_idx][col_idx] = _visited_tile_;
-								}
-								if (next == _obstruction_) {
-									// turn right, don't move
-									std::cout << "obstruction" << std::endl;		
-									map[row_idx][col_idx] = _guard_left_;
-								}
-								return result::incremented;
-							}
-							return result::ended;
-						}
-					default:
-						std::cout << "wtf." << std::flush;
+		void find_start() {
+			for (int row = 0; row != _rsize; ++row) {
+				for (int col = 0; col != _rsize; ++col) {
+					const char candidate = _map[row][col];	
+					if (guard_char_directions.count(candidate) != 0) {
+						_current.dir = guard_char_directions.at(candidate);
+						_current.row = row;
+						_current.col = col;
+					}
 				}
 			}
 		}
-	}
-	std::terminate();
-}
 
-
-starting_pos_and_orientation get_starting_pos_and_orientation(const std::vector<std::string> initial_map) {
-	starting_pos_and_orientation spao;
-	const int row_size = static_cast<int>(initial_map.size());
-	const int col_size = static_cast<int>(initial_map[0].size());
-	for (int row_idx = 0; row_idx != row_size; ++row_idx) {
-		for (int col_idx = 0; col_idx != col_size; ++col_idx) {
-			const auto symbol = initial_map[row_idx][col_idx];
-			const auto guard_symbol_it = std::find(_guard_symbols_.cbegin(), _guard_symbols_.cend(), symbol);
-			if (guard_symbol_it != _guard_symbols_.cend()) {
-				spao.row = row_idx;
-				spao.col = col_idx;
-				const char guard_symb = *guard_symbol_it;
-				switch(guard_symb) {
-					case _guard_up_:
-						{							
-							spao.ori = orientation::up;	
-							break;
-						}
-					case _guard_down_:
-						{							
-							spao.ori = orientation::down;	
-							break;
-						}
-					case _guard_left_:
-						{							
-							spao.ori = orientation::left;	
-							break;
-						}
-					case _guard_right_:
-						{							
-							spao.ori = orientation::right;	
-							break;
-						}
+		place get_next(const direction where) {
+			place next = _current;
+			switch(where) {
+				case direction::up: {
+					next.row--;
+					break;
 				}
+				case direction::down: {
+					next.row++;
+					break;
+				}
+				case direction::left: {
+					next.col--;
+					break;
+				}
+				case direction::right: {
+					next.col++;
+					break;
+				}
+				default:
+					std::terminate();
+			}
+			return next;
+		}
+
+		bool in_the_map(const place &to){
+			return (to.col >= 0 && to.row >= 0 && to.col < _csize && to.row < _rsize);
+		}
+
+		bool is_looping(const place &to) {
+			return std::any_of(
+					_history.cbegin(), 
+					_history.cend(), 
+					[&](const place &pl) {
+						return (pl.dir == to.dir && pl.row == to.row &&	pl.col == to.col);
+					}
+				);
+		}
+
+		void move(const place& next){
+			_current = next;
+		}
+
+		bool is_obstructed(const place &pl) {
+			return _map[pl.row][pl.col] == _obstruction_;
+		}
+
+		void rotate() {
+			switch(_current.dir) {
+				case direction::up:
+					{
+					_current.dir = direction::right;
+					return;
+					}
+				case direction::right:
+					{
+					_current.dir = direction::down;
+					return;
+					}
+				case direction::down:
+					{
+					_current.dir = direction::left;
+					return;
+					}
+				case direction::left:
+					{
+					_current.dir = direction::up;
+					return;
+					}
+				default:
+					std::terminate();
 			}
 		}
-	}
-	return spao;
-}
+
+		result move() {
+			_history.push_back(_current);
+			const place next = get_next(_current.dir);
+			if (is_looping(next)) {
+				return result::loop;
+			}
+			if (!in_the_map(next)) {
+				return result::ended;
+			}
+			if (is_obstructed(next)) {
+				rotate();
+			}
+			else {
+				move(next);
+			}
+			return result::moved;
+		}
+
+	private:
+		place _current;
+		std::vector<place> _history;
+		int _rsize;
+		int _csize;
+		std::vector<std::string> _map;
+};
 
 int main() {
-	/* std::vector<std::string> map; */
-	/* std::string line; */
-	/* std::ifstream file("input.txt"); */
+	std::vector<std::string> map;
+	std::string line;
+	std::ifstream file("input.txt");
 
-	/* if (file.is_open()) { */
-	/* 	while (std::getline(file, line)) { */
-	/* 			map.push_back(line); */
-	/* 	} */
-	/* 	file.close(); */
-	/* } else { */
-	/* 	std::cerr << "Unable to open file" << std::endl; */
-	/* 	return 1; */
-	/* } */
+	if (file.is_open()) {
+		while (std::getline(file, line)) {
+				map.push_back(line);
+		}
+		file.close();
+	} else {
+		std::cerr << "Unable to open file" << std::endl;
+		return 1;
+	}
 
-	std::vector<std::string> map {
-		"....#.....",
-		".........#",
-		"..........",
-		"..#.......",
-		".......#..",
-		"..........",
-		".#..^.....",
-		"........#.",
-		"#.........",
-		"......#...",
-	};
+	/* std::vector<std::string> map { */
+	/* 	"....#.....", */
+	/* 	".........#", */
+	/* 	"..........", */
+	/* 	"..#.......", */
+	/* 	".......#..", */
+	/* 	"..........", */
+	/* 	".#..^.....", */
+	/* 	"........#.", */
+	/* 	"#.........", */
+	/* 	"......#...", */
+	/* }; */
 
 	std::vector<std::vector<std::string>> maps_with_extra_obstructions;
 	const int row_size = static_cast<int>(map.size());
@@ -279,42 +195,25 @@ int main() {
 	for (int row_idx = 0; row_idx != row_size; ++row_idx) {
 		for (int col_idx = 0; col_idx != col_size; ++col_idx) {
 			if (map[row_idx][col_idx] == _clear_tile_) {
-				std::cout << "generating map with obstruction at (" << row_idx << ", " << col_idx << std::endl;
 				std::vector<std::string> map_with_obstruction = map;
-				map_with_obstruction[row_idx][col_idx] = _my_obstruction_;
+				map_with_obstruction[row_idx][col_idx] = _obstruction_;
 				maps_with_extra_obstructions.push_back(map_with_obstruction);
 			}
 		}
 	}
 	
 	int num_loop_positions {0};
-	for (auto mod_map: maps_with_extra_obstructions) {
-		const auto spao = get_starting_pos_and_orientation(mod_map);
-		print_map(mod_map);
-		auto result = result::incremented;
-		bool starting = true;
-		while (result == result::incremented) {
-			result = increment_guard_position(mod_map, spao, &starting);
-			starting = false;
-			print_map(mod_map);
-		}
-		if (result == result::loop) {
-			num_loop_positions++;
-			std::cout << "loops so far: " << num_loop_positions << std::endl;
-		}
+
+	for (auto &modmap : maps_with_extra_obstructions) {
+			auto res = result::moved;
+			guard grd(modmap);
+			while(res == result::moved){
+				res = grd.move();	
+			}
+			if (res == result::loop) {
+				num_loop_positions++;
+			}
 	}
+
 	std::cout << "num_loop_positions: " << num_loop_positions << std::endl;
-
-	/* print_map(map); */
-
-	const int num_positions_visited = std::accumulate(
-		map.cbegin(), 
-		map.cend(), 
-		1,
-		[&](int sum, const std::string &col) -> int { 
-			return sum + std::count(col.cbegin(), col.cend(), _visited_tile_);	
-		}
-	);
-
-	/* std::cout << "num_positions_visited: " << num_positions_visited << std::endl; */
 }
